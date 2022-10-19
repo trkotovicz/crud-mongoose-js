0e8fc31815e01fcdd20d2cbfb1e7e56abdca8b1f
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-10-19 18:30:11 -0300

    feat: adiciona o middleware de erro

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 require('express-async-errors');
 const express = require('express');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 module.exports = app;
